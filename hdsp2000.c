2841a28999b49bdfe407c311418f7866543620cc
David Bell <david.bell307@ntlworld.com>
2026-05-13 19:45:49 +0100

    Conway game of life on the display!

--- hdsp2000.c
+++ hdsp2000.c
@@ -12,7 +12,7 @@
 volatile uint col_index;
 volatile uint32_t column_data[5];
 volatile uint32_t new_data[5];
-uint32_t xy_data[7];
+volatile uint32_t xy_data[7];
 
 
 void core1_entry() {
@@ -30,7 +30,7 @@ void core1_entry() {
             put_column_data(pio, 0, column_data[i]);
             while(!(pio0_hw->intr)) {};
             gpio_put(i, true);
-            sleep_ms(1);
+            sleep_ms(5);
             gpio_put(i, false);
             irq_clear(PIO0_IRQ_0);
         }
@@ -39,6 +39,11 @@ void core1_entry() {
 
 
 void xy_to_col_data() {
+    // zero new data
+    for (int i = 0; i < 5; i++) {
+        new_data[i] = 0u;
+    }
+
     for (int row = 4; row >= 0; row--) {
         for (int digit = 3; digit >= 0; digit--) {
             for (int i = 6; i >= 0; i--) {
@@ -46,6 +51,67 @@ void xy_to_col_data() {
             }
         }
     }
+}
+
+
+void write_test_pattern() {
+    xy_data[0] = 0b11111111110111011111;
+    xy_data[1] = 0b00100100001000100100;
+    xy_data[2] = 0b00100100001000000100;
+    xy_data[3] = 0b00100111000111000100;
+    xy_data[4] = 0b00100100000000100100;
+    xy_data[5] = 0b00100100001000100100;
+    xy_data[6] = 0b00100111110111000100;
+}
+
+
+void xy_conway_iterate(){
+    uint32_t xy_tmp[7] = {0u, 0u, 0u, 0u, 0u, 0u, 0u};
+    bool alive_bit = false;
+    uint32_t running_total = 0;
+
+    for (int y = 0; y < 7; y++) {
+        for (int x = 0; x < 20; x++) {
+            alive_bit = (xy_data[y] & (0x1u << x)) ? true : false;
+            running_total = ((xy_data[((y+1)%7)] & (0x1 << ((x+1)%20))) ? 0x1 : 0x0) +
+                            ((xy_data[((y+1)%7)] & (0x1 << x))          ? 0x1 : 0x0) +
+                            ((xy_data[((y+1)%7)] & (0x1 << ((x-1)%20))) ? 0x1 : 0x0) +
+                            ((xy_data[y]         & (0x1 << ((x+1)%20))) ? 0x1 : 0x0) +
+                            ((xy_data[y]         & (0x1 << ((x-1)%20))) ? 0x1 : 0x0) +
+                            ((xy_data[((y-1)%7)] & (0x1 << ((x+1)%20))) ? 0x1 : 0x0) +
+                            ((xy_data[((y-1)%7)] & (0x1 << x))          ? 0x1 : 0x0) +
+                            ((xy_data[((y-1)%7)] & (0x1 << ((x-1)%20))) ? 0x1 : 0x0) ;
+            if (alive_bit) {
+                if      (running_total == 3u) xy_tmp[y] = xy_tmp[y] | (0x1u << x);
+                else if (running_total == 2u) xy_tmp[y] = xy_tmp[y] | (0x1u << x);
+            } else {
+                if (running_total == 3u) xy_tmp[y] = xy_tmp[y] | (0x1u << x);
+            }
+        }
+    }
+    for (int i = 0; i < 7; i++) {
+        xy_data[i] = xy_tmp[i];
+    }
+}
+
+
+void randomise_xy_data(){
+    xy_data[0] = get_rand_32();
+    xy_data[1] = get_rand_32();
+    xy_data[2] = get_rand_32();
+    xy_data[3] = get_rand_32();
+    xy_data[4] = get_rand_32();
+    xy_data[5] = get_rand_32();
+    xy_data[6] = get_rand_32();
+}
+
+
+void push_to_display() {
+    column_data[0] = new_data[0];
+    column_data[1] = new_data[1];
+    column_data[2] = new_data[2];
+    column_data[3] = new_data[3];
+    column_data[4] = new_data[4];
 }
 
 
@@ -58,27 +124,24 @@ int main() {
 
     gpio_put_all(false);
     multicore_launch_core1(core1_entry);
-    bool alive;
 
     sleep_ms(500);
-    xy_data[0] = 0b11111111110111011111;
-    xy_data[1] = 0b00100100001000100100;
-    xy_data[2] = 0b00100100001000000100;
-    xy_data[3] = 0b00100111000111000100;
-    xy_data[4] = 0b00100100000000100100;
-    xy_data[5] = 0b00100100001000100100;
-    xy_data[6] = 0b00100111110111000100;
-
+    write_test_pattern();
     xy_to_col_data();
+    push_to_display();
+
     sleep_ms(500);
-    column_data[0] = new_data[0];
-    column_data[1] = new_data[1];
-    column_data[2] = new_data[2];
-    column_data[3] = new_data[3];
-    column_data[4] = new_data[4];
+    randomise_xy_data();
+    xy_to_col_data();
+    push_to_display();
 
     while (true) {
-        tight_loop_contents();       
+        xy_conway_iterate();
+        sleep_ms(500);
+        xy_to_col_data();
+        
+        push_to_display();
+        
     }
 }
 
